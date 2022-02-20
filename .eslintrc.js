2ec99b9e6cef2e8f5fa01d09f1afb104841a7109
MasuqaT <occar@hotmail.co.jp>
2022-02-20 22:41:34 +0900

    install vite and jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
-  },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:jest/recommended",
     "plugin:import/typescript",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+    },
   ],
 };
