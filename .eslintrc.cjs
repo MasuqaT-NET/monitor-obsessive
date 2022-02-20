a8ac80f4b82e47245d0e06c15570c3bc9f7d7ee1
MasuqaT <occar@hotmail.co.jp>
2022-02-20 23:09:12 +0900

    configure import/order

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,43 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:jest/recommended",
+    "plugin:import/recommended",
     "plugin:import/typescript",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
   ],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
   rules: {
     "react/react-in-jsx-scope": "off", // New JSX transform
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "parent", "sibling", "index", "object"],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+        warnOnUnassignedImports: true,
+      },
+    ],
   },
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
       parserOptions: {
         project: ["./tsconfig.json"],
       },
+      plugins: ["@typescript-eslint"],
       extends: [
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// eslint-disable-next-line import/no-unresolved
 import "virtual:windi.css";
-import "./index.css";
+
 import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
